8c5825d4cc642b10bfe178e7c5a7f4e0fb0346f8
HAFIZA-ASGHIA <hafizaasghia@gmail.com>
2024-09-01 13:32:58 +0500

    uptade

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,20 +7,20 @@ const Page = () => {
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
         <Image
           className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src="/home.jpg"
-          alt="Sarah Dayan"
+          src="/About.jfif"
+          alt="About"
           width="384"
           height="512"
         />
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
-            Home is where we feel most comfortable. Its the place with our favorite cozy spots like the soft sofa in the living room and the kitchen where we cook and chat. Our bedrooms are where we relax and sleep soundly. 
+              Homes are more than just buildings they are the backdrop to our lives filled with the echoes of laughter and the warmth of cherished memories.Each home reflects its inhabitants unique stories and personalities offering a sanctuary where we find comfort and belonging.
             </p>
           </blockquote>
           <figcaption className="font-medium">
-            <div className="text-red-500 dark:text-black-400">
-          About my Home
+            <div className="text-pink-500 dark:text-sky-400">
+              About
             </div>
             <div className="text-slate-700 dark:text-slate-500">
               Asghia Saeed
--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
         <Image
           className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src="/home.jpg"
+          src="/address.png"
           alt="Sarah Dayan"
           width="384"
           height="512"
@@ -15,12 +15,11 @@ const Page = () => {
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
-            Home is where we feel most comfortable. Its the place with our favorite cozy spots like the soft sofa in the living room and the kitchen where we cook and chat. Our bedrooms are where we relax and sleep soundly. 
-            </p>
+            An address is a fundamental piece of information that specifies the location of a person organization or place. It serves as a critical tool for navigation communication and the delivery of goods and services.            </p>
           </blockquote>
           <figcaption className="font-medium">
-            <div className="text-red-500 dark:text-black-400">
-          About my Home
+            <div className="text-yellow-500 dark:text-sky-400">
+              Address
             </div>
             <div className="text-slate-700 dark:text-slate-500">
               Asghia Saeed
